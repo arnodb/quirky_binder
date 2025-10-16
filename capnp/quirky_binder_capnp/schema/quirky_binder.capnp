aecd81fa53dec03dd4260f5fded64ba069d2f8c8
Arnaud de Bossoreille <arnaud.debossoreille@gmail.com>
2025-10-16 22:03:59 +0200

    Add not statuses to teleoperations

--- capnp/quirky_binder_capnp/schema/quirky_binder.capnp
+++ capnp/quirky_binder_capnp/schema/quirky_binder.capnp
@@ -2,11 +2,11 @@
 
 interface State {
     graph @0 () -> (graph :Graph);
+    nodeStatuses @1 () -> (statuses :List(NodeStatusItem));
 }
 
 struct Graph {
     nodes @0 :List(Node);
-
     edges @1 :List(Edge);
 }
 
@@ -19,4 +19,18 @@ struct Edge {
     tailIndex @1 :UInt8;
     headName @2 :Text;
     headIndex @3 :UInt8;
+}
+
+struct NodeStatusItem {
+    nodeName @0 :Text;
+    state @1 :NodeState;
+    inputRead @2 :List(UInt32);
+    outputWritten @3 :List(UInt32);
+}
+
+struct NodeState {
+    union {
+        good @0 :Void;
+        error @1 :Text;
+    }
 }
--- capnp/quirky_binder_capnp/src/state.rs
+++ capnp/quirky_binder_capnp/src/state.rs
@@ -1,4 +1,5 @@
 use capnp::capability::Promise;
+use capnp_rpc::pry;
 use quirky_binder_support::prelude::*;
 
 use crate::quirky_binder_capnp;
@@ -62,6 +63,57 @@ where
             edge.set_tail_index(e.tail_index.try_into().unwrap());
             edge.set_head_name(e.head_name);
             edge.set_head_index(e.head_index.try_into().unwrap());
+        }
+
+        Promise::ok(())
+    }
+
+    fn node_statuses(
+        &mut self,
+        _: quirky_binder_capnp::state::NodeStatusesParams,
+        mut results: quirky_binder_capnp::state::NodeStatusesResults,
+    ) -> capnp::capability::Promise<(), capnp::Error> {
+        let length: usize = self
+            .chain_status
+            .as_ref()
+            .statuses()
+            .map(|thread_status| thread_status.lock().expect("lock").node_statuses().count())
+            .sum();
+
+        let mut statuses = results.get().init_statuses(length.try_into().unwrap());
+
+        let mut i = 0;
+        for thread_status in self.chain_status.as_ref().statuses() {
+            let thread_status = thread_status.lock().expect("lock");
+            for s in thread_status.node_statuses() {
+                let mut status = statuses.reborrow().get(i);
+
+                status.set_node_name(s.node_name.to_owned());
+                match s.state {
+                    NodeState::Good => {
+                        status.reborrow().init_state().set_good(());
+                    }
+                    NodeState::Error(err) => {
+                        status.reborrow().init_state().set_error(err);
+                    }
+                }
+                pry!(status.set_input_read(
+                    &*s.input_read
+                        .iter()
+                        .copied()
+                        .map(|a| u32::try_from(a).unwrap())
+                        .collect::<Vec<_>>(),
+                ));
+                pry!(status.set_output_written(
+                    &*s.output_written
+                        .iter()
+                        .copied()
+                        .map(|a| u32::try_from(a).unwrap())
+                        .collect::<Vec<_>>(),
+                ));
+
+                i += 1;
+            }
         }
 
         Promise::ok(())
