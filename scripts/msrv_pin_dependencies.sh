9e660d3540a9aa6d4d85107b736fce367eb7362b
Arnaud de Bossoreille <arnaud.debossoreille@gmail.com>
2025-10-26 14:38:44 +0100

    Pin home for MSRV

--- scripts/msrv_pin_dependencies.sh
+++ scripts/msrv_pin_dependencies.sh
@@ -5,6 +5,7 @@ set -eu
 set -x
 
 #cargo update -p abc --precise x.y.z
+cargo update -p home --precise 0.5.11
 
 cd codegen/quirky_binder_codegen_wasm
 #cargo update -p abc --precise x.y.z
@@ -12,4 +13,5 @@ cd -
 
 cd recipes
 #cargo update -p abc --precise x.y.z
+cargo update -p home --precise 0.5.11
 cd -
