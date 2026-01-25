bf83ccf08423d4075705ed8e4d7e6b818430954e
Arnaud de Bossoreille <arnaud.debossoreille@gmail.com>
2026-01-25 16:47:07 +0100

    Basic process discovery

--- capnp/quirky_binder_capnp/src/lib.rs
+++ capnp/quirky_binder_capnp/src/lib.rs
@@ -5,19 +5,33 @@ pub mod state;
 // Until further developments, quirky_binder declares that teleop is only supported on "linux".
 
 #[cfg(target_os = "linux")]
-pub use linux::run_chain;
+pub use linux::{discover_processes, run_chain};
 #[cfg(not(target_os = "linux"))]
-pub use no_teleop::run_chain;
+pub use no_teleop::{discover_processes, run_chain};
+
+#[derive(Clone, Debug)]
+pub struct Process {
+    pub pid: u32,
+    pub description: String,
+}
 
 #[cfg(target_os = "linux")]
 mod linux {
     use std::{
+        fs::File,
+        path::PathBuf,
         pin::pin,
+        str::FromStr,
         sync::{Arc, LazyLock},
         time::{Duration, Instant},
     };
 
     use futures::{task::LocalSpawnExt, AsyncReadExt, FutureExt, StreamExt};
+    pub use nix::unistd::Pid;
+    use nix::{
+        sys::signal::kill,
+        unistd::{getuid, User},
+    };
     use quirky_binder_support::status::DynChainStatus;
     use smol::{lock::Mutex, net::unix::UnixStream, Timer};
     use teleop::{
@@ -26,7 +40,21 @@ mod linux {
         operate::capnp::{run_server_connection, teleop_capnp, TeleopServer},
     };
 
-    use crate::{quirky_binder_capnp, state::StateServer};
+    use crate::{quirky_binder_capnp, state::StateServer, Process};
+
+    fn process_directory() -> Result<PathBuf, Box<dyn std::error::Error>> {
+        let uid = getuid();
+        let user = User::from_uid(uid)?.ok_or("Unable to resolve current user")?;
+        Ok(std::env::temp_dir().join(format!("quirky_binder_{}", user.name)))
+    }
+
+    fn reveal_process() -> Result<AutoDropFile, Box<dyn std::error::Error>> {
+        let dir = process_directory()?;
+        std::fs::create_dir_all(&dir)?;
+        let pid = std::process::id();
+        let file = dir.join(pid.to_string());
+        Ok(AutoDropFile::create(file)?)
+    }
 
     pub fn run_chain<S, J, E>(
         chain_status: S,
@@ -38,13 +66,12 @@ mod linux {
         E: Into<Box<dyn std::error::Error>> + Send + 'static,
         Box<dyn std::error::Error>: From<E>,
     {
-        let pid = std::process::id();
-        println!("PID: {pid}");
-
         let mut exec = futures::executor::LocalPool::new();
         let spawn = exec.spawner();
 
         exec.run_until(async {
+            let _reveal_process = reveal_process()?;
+
             let end_of_chain_token = CancellationToken::new();
 
             let join_main = std::thread::spawn({
@@ -175,11 +202,80 @@ mod linux {
 
         Ok(())
     }
+
+    pub fn discover_processes() -> Result<Vec<Process>, Box<dyn std::error::Error>> {
+        let dir = process_directory()?;
+        let entries = std::fs::read_dir(dir);
+        Ok(entries
+            .ok()
+            .map(|entries| {
+                entries
+                    .filter_map(|entry| {
+                        entry
+                            .ok()
+                            .filter(|entry| entry.path().is_file())
+                            .and_then(|entry| {
+                                let pid = u32::from_str(
+                                    &entry.path().file_name().unwrap().to_string_lossy(),
+                                )
+                                .ok();
+                                pid.map(|pid| {
+                                    // Set the description to the executable name at this time.
+                                    let mut path = PathBuf::new();
+                                    path.push("/proc");
+                                    path.push(pid.to_string());
+                                    path.push("exe");
+                                    Process {
+                                        pid,
+                                        description: std::fs::read_link(path)
+                                            .ok()
+                                            .and_then(|path| {
+                                                path.file_name()
+                                                    .map(|name| name.to_string_lossy().to_string())
+                                            })
+                                            .unwrap_or_default(),
+                                    }
+                                })
+                            })
+                            .filter(
+                                |&Process {
+                                     pid,
+                                     description: _,
+                                 }| {
+                                    i32::try_from(pid)
+                                        .ok()
+                                        .is_some_and(|pid| kill(Pid::from_raw(pid), None).is_ok())
+                                },
+                            )
+                    })
+                    .collect::<Vec<Process>>()
+            })
+            .unwrap_or_default())
+    }
+
+    struct AutoDropFile(PathBuf);
+
+    impl AutoDropFile {
+        pub fn create(path: PathBuf) -> std::io::Result<Self> {
+            File::create(&path)?;
+            Ok(Self(path))
+        }
+    }
+
+    impl Drop for AutoDropFile {
+        fn drop(&mut self) {
+            if self.0.exists() {
+                std::fs::remove_file(&self.0).unwrap();
+            }
+        }
+    }
 }
 
 #[cfg(not(target_os = "linux"))]
 mod no_teleop {
     use quirky_binder_support::status::DynChainStatus;
+
+    use crate::Process;
 
     pub fn run_chain<S, J, E>(
         _chain_status: S,
@@ -192,5 +288,9 @@ mod no_teleop {
         Box<dyn std::error::Error>: From<E>,
     {
         Ok(join_chain()?)
+    }
+
+    pub fn discover_processes() -> Result<Vec<Process>, Box<dyn std::error::Error>> {
+        Ok(Vec::new())
     }
 }
